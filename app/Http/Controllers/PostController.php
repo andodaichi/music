4a0a20c66565b35ea0d5bf8ae5acde14d98a72d3
andodaichi <c0p31004@bunkyo.ac.jp>
2023-01-18 21:56:39 +0900

    リレーション

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,11 +5,50 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\Post;
+use App\Http\Requests\PostRequest;
+use App\Models\Category;
 
 class PostController extends Controller
 {
-   public function index(Post $post)
+   public function index()
     {
-        return Inertia::render("Post/Index",["posts" => $post->get()]);
+        return Inertia::render("Post/Index",["posts" => Post::with("category")->get()]);
     }
+    
+    public function show(Post $post)
+    {
+        return inertia("Post/Show", ["post" => $post->load('category')]);
+    }
+    
+    public function create(Category $category)
+    {
+        return Inertia::render("Post/Create",["categories" => $category->get()]);
+    }
+    
+    public function store(PostRequest $request, Post $post)
+    {
+        $input = $request->all();
+        $post->fill($input)->save();
+        
+    }
+    
+    public function edit(Post $post)
+    {
+        return Inertia::render("Post/Edit", ["post" => $post]);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        $input = $request->all();
+        $post->fill($input)->save();
+        return redirect("/posts/" . $post->id);
+    }
+    
+    public function delete(Post $post)
+    {
+        $post->delete();
+        return redirect("/posts");
+    }
+    
+    
 }
---
+++ resources/js/Pages/Post/Show.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Inertia } from "@inertiajs/inertia";
+import { Link } from '@inertiajs/inertia-react'
+
+import Authenticated from "@/Layouts/AuthenticatedLayout";
+
+const Show = (props) => {
+    const { post } = props;
+
+    return (
+        <Authenticated auth={props.auth} header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    Show
+                </h2>
+            }>
+            
+            <div className="p-12">
+                <h1>{ post.title }</h1>
+                
+                <div>
+                    <h3>本文</h3>
+                    <p>{ post.body }</p>
+                </div>
+                
+                <div>
+                    <p>{ post.category.name }</p>
+                </div>
+                
+                
+                <div>
+                    [<Link href={`/posts/${post.id}/edit`}>edit</Link>]
+                </div>
+                
+              
+                <div>
+                    <Link href="/posts">戻る</Link>
+                </div>
+            </div>
+            
+        </Authenticated>
+        );
+}
+
+export default Show;
